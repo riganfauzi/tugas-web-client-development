0706e7be1ea7891f549ef42db0edbc77e01bd68f
Rigan Nur Fauzi <riganfauzi@gmail.com>
2025-03-16 00:28:16 +0700

    feat: konversi centimeter ke kilometer dan sebaliknya

---
+++ Tugas2/Javascript/centimeter_to_kilometer.js
@@ -0,0 +1,4 @@
+function konversiCmKeKm(cm) {
+    return (cm / 100000) + " km";
+}
+console.log(konversiCmKeKm(100000)); // Output: 1 km
---
+++ Tugas2/Typescript/centimeter_to_kilometer.ts
@@ -0,0 +1,4 @@
+function konversiCentimeterKeKilometer(cm: number): string {
+    return (cm / 100000) + " km";
+}
+console.log(konversiCentimeterKeKilometer(100000)); // Output: 1 km
